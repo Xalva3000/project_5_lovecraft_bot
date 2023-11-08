b4cd676e96a368a8468bea73fd0bc5293cf12d18
Xalva <maasania@gmail.com>
2023-11-08 11:28:30 +0400

    исправления текстов и повторов в словаре

--- terms_plus_translation_script.py
+++ terms_plus_translation_script.py
@@ -9,14 +9,20 @@ lst = []
 with open("books/dct_plus_translation.txt", "rt", encoding="UTF-8") as file:
     content = [line.strip() for line in file.readlines()]
 
-
+print(len(content))
 for row in content:
-    if row and len(row) > 4:
+    if row:
         lst.append(row.split("\t", 2))
 
+print(len(lst))
+
 for k, t, v in lst:
+    if k in dct:
+        print(k, t, v)
     dct[k] = (k, t, v)
 
+
+print(len(dct))
 
 async def insert():
     await AsyncQuery.insert_kabdict_definition(dct)
